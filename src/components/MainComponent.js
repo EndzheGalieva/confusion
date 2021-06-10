96fdbfbdbfffecab31a874041ebaf4370cbb0353
Endzhe Galieva <endzhe91@gmail.com>
2021-06-10 11:23:51 +0300

    single page applications part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,29 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Home from './HomeComponent';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import {DISHES} from "../shared/dishes";
-import { Switch, Route, Redirect } from "react-router-dom";
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
-
-
 
   render() {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        />
       );
     }
 
@@ -31,8 +49,10 @@ class Main extends Component {
       <div>
         <Header/>
         <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer/>
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -2,12 +2,12 @@ export const DISHES =
   [
     {
       id: 0,
-      name: 'Uthappizza',
+      name:'Uthappizza',
       image: 'assets/images/uthappizza.png',
       category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffaio Paneer'
+      label:'Hot',
+      price:'4.99',
+      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
     },
     {
       id: 1,
@@ -20,20 +20,20 @@ export const DISHES =
     },
     {
       id: 2,
-      name: 'Vadonut',
+      name:'Vadonut',
       image: 'assets/images/vadonut.png',
       category: 'appetizer',
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+      label:'New',
+      price:'1.99',
+      description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
     },
     {
       id: 3,
-      name: 'ElaiCheese Cake',
+      name:'ElaiCheese Cake',
       image: 'assets/images/elaicheesecake.png',
       category: 'dessert',
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+      label:'',
+      price:'2.99',
+      description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
   ];
